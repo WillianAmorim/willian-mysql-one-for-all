d765909230cfca2d18ed710d8b3deae521879597
Willian <willian.amorim1@outlook.com>
2022-06-09 15:59:46 -0300

    feito requisito 6

---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+SELECT
+  MIN(p.price) AS "faturamento_minimo",
+  MAX(p.price) AS "faturamento_maximo",
+  ROUND(AVG(p.price), 2) AS "faturamento_medio",
+  ROUND(SUM(p.price), 2) AS "faturamento_total"
+FROM
+  SpotifyClone.plan AS p
+  INNER JOIN SpotifyClone.user as u ON p.plan_id = u.plan_id;
